35a65835fc21f876ae0eb003207daf819882e1e1
ylan <ylan@invoo.com.cn>
2020-04-20 14:53:01 +0800

    新增组件: CupertinoContextMenu,使用方法后续更新

---
+++ lib/tabbar/mine/widgets/c/cupertino_context_menu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CupertinoContextMenuPage extends StatefulWidget {
+  @override
+  _CupertinoContextMenuPageState createState() => _CupertinoContextMenuPageState();
+}
+
+class _CupertinoContextMenuPageState extends State<CupertinoContextMenuPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("新增组件"),
+    );
+  }
+}
